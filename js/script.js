1d83d29bd4c1d93e72c51af0a244642323b6cf3f
swissert <40758949+swissert@users.noreply.github.com>
2018-07-16 15:45:32 +0200

    Animations

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("Hi! Welcome to my portfolio site")
+
+$(document).ready(function() {
+  $(".project").on("mouseenter", function() {
+    $(this).find(".project-details").slideToggle();
+    });
+  $(".project").on("mouseleave", function() {
+    $(this).find(".project-details").fadeOut();
+    });
+});
